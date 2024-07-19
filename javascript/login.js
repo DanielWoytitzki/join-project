6f6ee9e862fa622b652e6532f5e85735bf5c36b5
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-07-19 14:03:03 +0200

    added files
    - contacts.html (updated)
    - contact.js
    - login.js
    - sign_up.js
    - login.html
    - style_contact.css (dummy)
    - style_login.css (dummy)

---
+++ javascript/login.js
@@ -0,0 +1,57 @@
+const BASE_URL = "https://join-7b4c8-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function logInAsUser() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+
+    let user = users.find(u => u.email == email.value && u.password == password.value);
+    console.log(user);
+    if (user) {
+        console.log('User gefunden');
+    } else {
+        console.log('User wurde nicht gefunden. Bitte registrieren Sie sich oder melden sich als Gast an');
+    }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+
+if (msg) {
+    msgBox.innerHTML = msg;
+} else {
+    // display: none
+}
+
+function checkRememberMe() {
+    let checkbox = document.getElementById('checkbox');
+
+    if (checkbox == checked) {
+        console.log('Log in wird beim nächsten Mal angezeigt');
+        saveLogInToLocalStorage();
+        loadLogInFromLocalStorage();
+    } else {
+        console.log('Log in muss erneut eingegeben werden');
+        document.getElementById('email').value = '';
+        document.getElementById('password').value = '';
+    }
+}
+
+function saveLogInToLocalStorage() {
+    let emailAsText = JSON.stringify(email);
+    let passwordAsText = JSON.stringify(password);
+    localStorage.setItem('email', emailAsText);
+    localStorage.setItem('password', passwordAsText);
+}
+
+function loadLogInFromLocalStorage() {
+    let emailAsText = localStorage.getItem('email');
+    let passwordAsText = localStorage.getItem('password');
+    if (emailAsText && passwordAsText) {
+        email = JSON.parse(emailAsText);
+        password = JSON.parse(passwordAsText);
+    }
+}
+
+function logInAsGuest() {
+
+}
---
+++ javascript/sign_up.js
@@ -0,0 +1,59 @@
+const BASE_URL = "https://join-7b4c8-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function addUserToDatabase() {
+    let userName = document.getElementById('name').value;
+    let userEmail = document.getElementById('email').value;
+    let userPassword = document.getElementById('password').value;
+
+    let user = {
+        name: userName,
+        email: userEmail,
+        password: userPassword,
+    };
+
+    let response = await fetch(BASE_URL + "/users.json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user)
+    });
+    let responseAsJson = await response.json();
+    console.log(responseAsJson);
+
+    checkConfirmedPassword(userPassword);
+}
+
+function checkConfirmedPassword(userPassword) {
+    let confirmedPassword = document.getElementById('confirmedPassword');
+
+    if (userPassword.value == confirmedPassword.value) {
+        console.log('Passwort stimmt überein');
+    } else {
+        console.log('Bitte überprüfe Sie Ihre Eingabe.');
+    }
+}
+
+function checkCheckbox() {
+    let checkbox = document.getElementById('checkbox');
+    
+    if (checkbox == checked) {
+        console.log('Privacy policy wurde akzeptiert');
+        document.getElementById('signup-button').disabled = true;
+    } else {
+        console.log('Bitte Privacy policy akzeptieren');
+        document.getElementById('signup-button').disabled = false;
+    }
+}
+
+function signUp() {
+    checkConfirmedPassword();
+    checkCheckbox();
+    addUserToDatabase();
+    
+    document.getElementById('name').value = '';
+    userEmail = document.getElementById('email').value = '';
+    userPassword = document.getElementById('password').value = '';
+    
+    window.location.href = 'login.html?msg=You Signed Up successfully'
+}
