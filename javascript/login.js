0a0e18ac14441916d1ff275020a45393eaf7a8ce
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-07-21 11:43:10 +0200

    updated file
    - login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,34 +1,51 @@
 const BASE_URL = "https://join-7b4c8-default-rtdb.europe-west1.firebasedatabase.app/";
 
-async function logInAsUser() {
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
+/**
+ * Adds an eventlistener in order to load the function "fetchUserDetailsFromLocalStorage()" after the whole site has been loaded
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetchUserDetailsFromLocalStorage();
+});
 
-    let user = users.find(u => u.email == email.value && u.password == password.value);
-    console.log(user);
-    if (user) {
+/**
+ * This function logs one in as user as long as one is found in the database
+ */
+async function logInAsUser() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let response = await fetch(BASE_URL + "users.json");
+    let users = await response.json();
+    console.log(users);
+
+    let userFound = false;
+    for (let userId in users) {
+        if (users[userId].email === email && users[userId].password === password) {
+            userFound = true;
+            console.log('User gefunden:', users[userId]);
+            break;
+        }
+    }
+
+    if (userFound) {
         console.log('User gefunden');
     } else {
         console.log('User wurde nicht gefunden. Bitte registrieren Sie sich oder melden sich als Gast an');
     }
+    checkRememberMe(email, password);
+    window.location.href = 'summary.html';
 }
 
-const urlParams = new URLSearchParams(window.location.search);
-const msg = urlParams.get('msg');
+/**
+ * This function checks if the checkbox of "Remember me" is checked or not
+ */
+function checkRememberMe(email, password) {
+    let checkbox = document.getElementById('checkbox-remember-me').checked;
 
-if (msg) {
-    msgBox.innerHTML = msg;
-} else {
-    // display: none
-}
-
-function checkRememberMe() {
-    let checkbox = document.getElementById('checkbox');
-
-    if (checkbox == checked) {
+    if (checkbox == true) {
         console.log('Log in wird beim nächsten Mal angezeigt');
-        saveLogInToLocalStorage();
-        loadLogInFromLocalStorage();
+        saveLogInToLocalStorage(email, password);
+        loadLogInFromLocalStorage(email, password);
     } else {
         console.log('Log in muss erneut eingegeben werden');
         document.getElementById('email').value = '';
@@ -36,14 +53,26 @@ function checkRememberMe() {
     }
 }
 
-function saveLogInToLocalStorage() {
+/**
+ * This function saves ones email and password to the local storage if the checkbox of "Remember me" is/was checked
+ * 
+ * @param {string} email - Email of the current user
+ * @param {string} password - Password of the current user
+ */
+function saveLogInToLocalStorage(email, password) {
     let emailAsText = JSON.stringify(email);
     let passwordAsText = JSON.stringify(password);
     localStorage.setItem('email', emailAsText);
     localStorage.setItem('password', passwordAsText);
 }
 
-function loadLogInFromLocalStorage() {
+/**
+ * This function loads ones email and password from the local storage if the checkbox of "Remember me" is/was checked
+ * 
+ * @param {string} email - Email of the current user
+ * @param {string} password - Password of the current user
+ */
+function loadLogInFromLocalStorage(email, password) {   
     let emailAsText = localStorage.getItem('email');
     let passwordAsText = localStorage.getItem('password');
     if (emailAsText && passwordAsText) {
@@ -52,10 +81,37 @@ function loadLogInFromLocalStorage() {
     }
 }
 
-function logInAsGuest() {
+/**
+ * This function fetchs ones email and password from the local storage if the checkbox of "Remember me" is/was checked
+ */
+function fetchUserDetailsFromLocalStorage() {
+    let inputEmail = document.getElementById('email');
+    let inputPassword = document.getElementById('password');
 
+    if (inputEmail && inputPassword) {
+        let email = localStorage.getItem('email');
+        let password = localStorage.getItem('password');
+
+        if (email !== null) {
+            email = email.replace(/"/g, '');
+            inputEmail.value = email;
+        }
+        if (password !== null) {
+            password = password.replace(/"/g, '');
+            inputPassword.value = password;
+        }
+    } else {
+        console.error('Email oder Password nicht gefunden');
+    }
 }
 
+function logInAsGuest() {
+    window.location.href = 'summary.html';
+}
+
+/**
+ * This function forwards one to the sign-up page
+ */
 function forwardToSignUp() {
     window.location.href = 'sign_up.html';
 }
