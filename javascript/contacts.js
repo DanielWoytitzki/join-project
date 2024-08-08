8621aa2dc55e429eaa6cef88d17d5e9aaf61ebaf
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-08 06:44:02 +0200

    changed some files
    - contacts.html
    - contacts.js

--- javascript/contacts.js
+++ javascript/contacts.js
@@ -3,21 +3,11 @@ const BASE_URL = "https://join-7b4c8-default-rtdb.europe-west1.firebasedatabase.
 */
 
 var colorArray = [
-  "#FF7A00",
-  "#FF5EB3",
-  "#6E52FF",
-  "#9327FF",
-  "#00BEE8",
-  "#1FD7C1",
-  "#FF745E",
-  "#FFA35E",
-  "#FC71FF",
-  "#FFC701",
-  "#0038FF",
-  "#C3FF2B",
-  "#FFE62B",
-  "#FF4646",
-  "#FFBB2B",
+  "#FF7A00", "#FF5EB3", "#6E52FF",
+  "#9327FF", "#00BEE8", "#1FD7C1",
+  "#FF745E", "#FFA35E", "#FC71FF",
+  "#FFC701", "#0038FF", "#C3FF2B",
+  "#FFE62B", "#FF4646", "#FFBB2B",
 ];
 
 loadContactsFromDatabase();
